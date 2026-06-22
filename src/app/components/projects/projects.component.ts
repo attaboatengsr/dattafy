3ad0f199f6cac2caec16f2ac14faa0f77c07a7b3
Atta <attasenior@gmail.com>
2026-06-22 19:53:48 -0400

    updated the first project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-projects',
   imports: [
               MatCardModule,
               MatButtonModule,
+              MatTooltipModule,
+              MatChipsModule,
            ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
